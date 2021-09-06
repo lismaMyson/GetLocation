1af89b35f3a6844457c21f57e5fcc2a0c4f59edd
pranavkp1661 <pranavp7645357@gmail.com>
2021-09-06 13:41:09 +0530

    location done

---
+++ app/src/main/java/com/example/location/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.location;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    Button btLocation;
+    TextView tvLati;
+    TextView tvLati1;
+    TextView tvLong;
+    TextView tvLong1;
+    TextView tvAddresses;
+    Context context;
+    FusedLocationProviderClient fusedLocationProviderClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        context=this;
+        btLocation = findViewById(R.id.btLocation);
+        tvLati = findViewById(R.id.tvLati);
+        tvLati1 = findViewById(R.id.tvLati1);
+        tvLong = findViewById(R.id.tvLong);
+        tvAddresses=findViewById(R.id.tvAddresses);
+        tvLong1 = findViewById(R.id.tvLong1);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        btLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                    getLocation();
+                }else {
+                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},44);
+                }
+
+            }
+
+            @SuppressLint("MissingPermission")
+            private void getLocation() {
+                fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        Location location=task.getResult();
+                        if (location != null){
+                            Geocoder geocoder=new Geocoder(MainActivity.this, Locale.getDefault());
+                            try {
+                                List<Address> addresses=geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+                                tvLati.setText("latitude="+location.getLatitude());
+                                tvLong.setText("Longitude="+location.getLongitude());
+                                tvAddresses.setText("Address="+addresses.get(0).getAddressLine(0));
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }else{
+                            Toast t=Toast.makeText(context,"location not get",Toast.LENGTH_SHORT);
+                            t.show();
+
+                        }
+                    }
+                });
+            }
+        });
+
+    }
+}
