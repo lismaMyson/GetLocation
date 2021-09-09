0410531b17c23376d75ae87748b650d3e171fccb
lismaMyson <lismamyson2000@gmail.com>
2021-09-09 15:25:22 +0530

    something

--- app/src/main/java/com/example/location/DesignActivity.java
+++ app/src/main/java/com/example/location/DesignActivity.java
@@ -2,13 +2,28 @@ package com.example.location;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DesignActivity extends AppCompatActivity {
+    Button btSettings;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        context=this;
         setContentView(R.layout.activity_design);
+        btSettings=findViewById(R.id.btSettings);
+        btSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(context,DemoActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
